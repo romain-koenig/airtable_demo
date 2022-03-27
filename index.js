fc696395b0c70df8b1facdc86b33a2618f3eae7c
Romain KOENIG <romain.koenig@gmail.com>
2022-03-27 22:54:27 +0200

    getting more infos

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { logging } = require("./libs/utils");
 
 const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
 
-
 // REAL TREATMENT STARTS HERE
 
 (async () => {
@@ -11,16 +10,31 @@ const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
 	var Airtable = require('airtable');
 	var base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base('app2mNi1K5e87VIF5');
 
+	const newsOfTheWeek = await base('News')
+		.select(
+			{
+				view: "NewsThisWeek"
+			}
+		)
+		.all();
 
-	const newsOfTheWeek = await base('News').select(
-		{
-			view: "NewsThisWeek"
-		}
-	).all();
+	const teams = await base('Équipes')
+		.select()
+		.all();
 
+	await Promise.all(newsOfTheWeek.map(async (news) => {
 
-	newsOfTheWeek.forEach(news => {
 		logging(`News in the working list : ${news.get('Name')}`);
+		const project = news.get('Projects');
+
+		const projectDetails = await base('Projects').find(project);
+		logging(`Project for ${news.get('Name')} : ${projectDetails.get('Name')}`);
+	}));
+
+
+
+	teams.forEach(team => {
+		logging(`Teams : ${team.get('Name')}`)
 	});
 
 })();
