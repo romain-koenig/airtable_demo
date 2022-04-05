bce5afb93e951bb157027416b74948507903ab45
Romain KOENIG <romain.koenig@gmail.com>
2022-04-05 10:35:18 +0200

    Starting back from the basics

--- index.js
+++ index.js
@@ -2,39 +2,36 @@ require('dotenv').config();
 const { logging } = require("./libs/utils");
 
 const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
+const BASE_ID = process.env.BASE_ID;
+const NEWS_TABLE_ID = process.env.NEWS_TABLE_ID;
+const NEWS_THIS_WEEK_VIEW = process.env.NEWS_THIS_WEEK_VIEW;
 
 // REAL TREATMENT STARTS HERE
 
 (async () => {
 
 	var Airtable = require('airtable');
-	var base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base('app2mNi1K5e87VIF5');
+	var base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(BASE_ID);
 
-	const newsOfTheWeek = await base('News')
-		.select(
-			{
-				view: "NewsThisWeek"
-			}
-		)
-		.all();
+	base(NEWS_TABLE_ID).select({
+		// Selecting the first 100 records in All News:
+		maxRecords: 100,
+		view: NEWS_THIS_WEEK_VIEW
+	}).eachPage(function page(records, fetchNextPage) {
+		// This function (`page`) will get called for each page of records.
 
-	const teams = await base('Équipes')
-		.select()
-		.all();
+		records.forEach(function (record) {
+			logging(`Retrieved ${record.get('Name')}`);
+			logging(JSON.stringify(record));
+		});
 
-	await Promise.all(newsOfTheWeek.map(async (news) => {
+		// To fetch the next page of records, call `fetchNextPage`.
+		// If there are more records, `page` will get called again.
+		// If there are no more records, `done` will get called.
+		fetchNextPage();
 
-		logging(`News in the working list : ${news.get('Name')}`);
-		const project = news.get('Projects');
-
-		const projectDetails = await base('Projects').find(project);
-		logging(`Project for ${news.get('Name')} : ${projectDetails.get('Name')}`);
-	}));
-
-
-
-	teams.forEach(team => {
-		logging(`Teams : ${team.get('Name')}`)
+	}, function done(err) {
+		if (err) { loggingError(err); return; }
 	});
 
 })();
