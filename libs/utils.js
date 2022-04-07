5306fc6e81102460f1026412728e6926e6ee98ea
Romain KOENIG <romain.koenig@gmail.com>
2022-04-07 11:18:00 +0200

    Adding writefiles

--- libs/utils.js
+++ libs/utils.js
@@ -1,5 +1,16 @@
+const fsLibrary = require('fs');
+
 const logging = content => console.log(content);
 exports.logging = logging;
 
 const loggingError = content => console.error(content);
 exports.logging = loggingError;
+
+const writeFile = (filePath, content) => {
+	fsLibrary.writeFile(filePath, content, (error) => {
+		// In case of a error throw err exception. 
+		if (error)
+			throw error;
+	});
+};
+exports.writeFile = writeFile;
