ebcd04638e1d4ed5e75dd140855ad52f47bac1d8
Romain KOENIG <romain.koenig@gmail.com>
2022-04-08 11:27:22 +0200

    Simple snippet

---
+++ templates/snippets/htmlSnippets.js
@@ -0,0 +1,30 @@
+const articleWithTeam = "<article>\
+	<h2 style=\"color: #25c78e; font-size: 28px; font-weight: 600; line-height: 32px; margin: 48px 0 12px 0; text-align: left;\">\
+		SUBJECT_TITLE\
+	</h2>\
+	<h3 style=\"color: 021F3D; top: 336px; left: 64px; width: 156px; height: 19px; text-align: left; font: Open Sans bold 14px/19px normal; color: #021F3D; opacity: 1;\">\
+		👬 TEAMS_HERE\
+	</h3>\
+	<p>\
+		DESCRIPTION_HERE\
+	</p>\
+	<p>\
+		SNAKE_LINKS_HERE\
+	</p>\
+	<hr style=\"color: #eaeaea;\">\
+	<\/article>";
+exports.articleWithTeam = articleWithTeam;
+
+const articleWithoutTeam = "<article>\
+	<h2 style=\"color: #25c78e; font-size: 28px; font-weight: 600; line-height: 32px; margin: 48px 0 12px 0; text-align: left;\">\
+		SUBJECT_TITLE\
+	</h2>\
+	<p>\
+		DESCRIPTION_HERE\
+	</p>\
+	<p>\
+		SNAKE_LINKS_HERE\
+	</p>\
+	<hr style=\"color: #eaeaea;\">\
+	<\/article>";
+exports.articleWithoutTeam = articleWithoutTeam;
