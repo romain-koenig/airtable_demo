7f78d8ca35b82cf9563c9a47846a21d11b801990
Romain KOENIG <romain.koenig@gmail.com>
2022-04-15 14:50:36 +0200

    Laying out the first bricks

--- src/generateMails.js
+++ src/generateMails.js
@@ -6,5 +6,35 @@ logging("Starting Mail generation");
 
 (async () => {
 
+	const templatesFile = await readFile("./data/templates.json");
+	const templates = JSON.parse(templatesFile);
 
-});
+
+	const recipientsFile = await readFile("./data/recipients.json");
+	const recipients = JSON.parse(recipientsFile);
+
+	logging(`Template file : ${templates}`);
+
+	templates.map(template => {
+
+		const currentRecipients = recipients.filter(recipient => {
+			logging(`Comparing ${template.teamName[0]} and ${recipient.teamName} : ${template.teamName[0] == recipient.teamName}`)
+			return template.teamName[0] == recipient.teamName;
+		});
+
+
+		// logging(`All recipients : ${JSON.stringify(recipients)}`);
+		logging(`Current (filtered) recipients : ${JSON.stringify(currentRecipients)}`);
+		// logging(`Templates : ${JSON.stringify(template)}`);
+
+
+		logging(
+			currentRecipients.map(recipient => {
+				return `${recipient.mail} - ${recipient.name} - ${recipient.teamShortName}`
+			})
+				.reduce((a, b) => a.concat(`${b}\n`), ""));
+
+	})
+
+
+})();
